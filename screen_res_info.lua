b8cb4a38ec38605108ba177d025dea90ed1e7933
Miłosz Gilga <gilgamilosz451@gmail.com>
2021-02-24 12:26:38 +0100

    Create screen_res_info.lua

---
+++ screen_res_info.lua
@@ -0,0 +1,7 @@
+-- Useful simple program to check quantity of monitor cells, 
+-- which can be use in many graphical and interactive programs
+
+m = peripheral.wrap("left")
+w,h = m.getSize()
+print(w)
+print(h)
